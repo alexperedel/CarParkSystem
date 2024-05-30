ab33613c3321866ef06c104af9871b19cbb854d2
ALEKSANDR PEREDELSKII <PEREDA@tdm.local>
2024-05-30 14:58:22 +0800

    Added constructors and attributes to the display and sensor classes

--- src/display.py
+++ src/display.py
@@ -1,2 +1,9 @@
 class Display:
-    pass
+    def __init__(self,car_park, id, message="", is_on=False):
+        self.id = id
+        self.message = message
+        self.is_on = is_on
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"ID: {self.id}, Message: {self}"
--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,15 @@
 class Sensor:
-    pass
+    def __init__(self, id, is_active, car_park)
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f'ID: {self.id}, Status: {self.is_active}'
+
+
+class EntrySensor(Sensor):
+    ...
+
+class ExitSensor(Sensor):
+    ...
